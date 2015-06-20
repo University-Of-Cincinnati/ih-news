0736e408946d6238b3d5cf65ef611849e6c5c5c1
hartmamt <hartmamt@Matthews-MacBook-Pro.local>
2015-06-20 07:28:11 -0400

    using airbnb style

--- src/components/IHNewsFeed.jsx
+++ src/components/IHNewsFeed.jsx
@@ -1,25 +1,37 @@
 import React from 'react/addons';
-import {Divider,Btn, BtnItem, Icon, List, ListItem, Card, CardItem, CardItemHeader, CardItemFooter, CardItemContent, Block, Text, Image} from 'react-essence';
 
+import {
+  Divider,
+  Btn,
+  BtnItem,
+  Icon,
+  List,
+  ListItem,
+  Card,
+  CardItem,
+  CardItemHeader,
+  CardItemFooter,
+  CardItemContent,
+  Block,
+  Text,
+  Image
+} from 'react-essence';
 
-//document.domain='uc.edu';
-
-var IHNewsFeed = React.createClass({
-
-  getMoreLink(){
-    if (this.props.ncd["news-publication-result"]["section-links"].link!==undefined){
-      return this.props.ncd["news-publication-result"]["section-links"].link.filter((item) => item.viewall === 'yes')[0].text;
+let IHNewsFeed = React.createClass({
+  getMoreLink() {
+    if (this.props.ncd['news-publication-result']['section-links'].link!==undefined){
+      return this.props.ncd['news-publication-result']['section-links'].link.filter((item) => item.viewall === 'yes')[0].text;
     }
   },
 
-  handleClick(){
+  handleClick() {
     //use PeopleSoft function to open URL in modal
     openContentInModal(this.getMoreLink());
   },
-  render() {
 
-    if (this.props.ncd["news-publication-result"]["section-links"].link!==undefined){
-      var moreButton =
+  render() {
+    if (this.props.ncd['news-publication-result']['section-links'].link!==undefined){
+      const moreButton =
       <BtnItem
           classes={'flat e-right'}
           label='More...'
@@ -31,20 +43,20 @@ var IHNewsFeed = React.createClass({
 
     return (
       <div>
-        <NewsCards data={this.props.ncd["news-publication-result"]["news-publication-top-story"].item} />
+        <NewsCards data={this.props.ncd['news-publication-result']['news-publication-top-story'].item} />
           {moreButton}
       </div>
     );
   }
+
 });
 
 
-var NewsCards = React.createClass({
+let NewsCards = React.createClass({
   render: function() {
-    var cardNodes = this.props.data.map(function (cardItem){
+    var cardNodes = this.props.data.map((cardItem) => {
       return (
         <NewsCardItem data={cardItem}/>
-
       );
     });
 
@@ -57,37 +69,35 @@ var NewsCards = React.createClass({
 });
 
 
-var NewsCardItem = React.createClass({
-  handleClick(){
+let NewsCardItem = React.createClass({
+  handleClick() {
     //use PeopleSoft function to open URL in modal
     openContentInModal(this.props.data.link);
   },
   render: function() {
-
     let cardItem = this.props.data;
-    var newsImage = null;
+    let newsImage = null;
 
-    if (cardItem.image !== undefined){
-      newsImage=<Block type="div" classes={"card-main-image e-paper e-background-white"}>
+    if (cardItem.image !== undefined) {
+      newsImage=<Block type='div' classes={'card-main-image e-paper e-background-white'}>
                   <Image src={cardItem.image.url}  alt={cardItem.image.title} />
                 </Block>;
     }
 
     return (
       <Card>
-        <CardItem classes={"e-background-white"}>
-          <CardItemContent classes={"card-supporting-text"}>
-            <Text type="h2" classes={"e-title"}>
+        <CardItem classes={'e-background-white'}>
+          <CardItemContent classes={'card-supporting-text'}>
+            <Text type='h2' classes={'e-title'}>
               {cardItem.title}
             </Text>
-            <Text type="h4">
+            <Text type='h4'>
               {cardItem.description}
             </Text>
           </CardItemContent>
           {newsImage}
           <CardItemFooter>
-
-            <Block type="div" classes={"e-right"}>
+            <Block type='div' classes={'e-right'}>
               <Btn>
                 <BtnItem
                     classes={'flat'}
@@ -103,7 +113,7 @@ var NewsCardItem = React.createClass({
       </Card>
     );
   }
-});
 
+});
 
 module.exports = IHNewsFeed;
